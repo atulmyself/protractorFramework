5b76bcb6059e0678116fb5f9176d3b44a2dd85e3
atulvvdn <atul.gupta@vvdntech.in>
2020-07-22 11:58:55 +0530

    tet

--- tests/loginPAGE.js
+++ tests/loginPAGE.js
@@ -1,9 +1,7 @@
 let loginpage = require('../pages/loginpage')
 
 describe('Login page test', function() {
-
     it('Open Login page test', function() {
-            
        loginpage.get('https://qa-dot-shipperbee-drivercentre.appspot.com');
         loginpage.enter_email('amit@yopmail.com');
         loginpage.enter_password('Pass@1234');
@@ -11,5 +9,4 @@ describe('Login page test', function() {
        browser.sleep(4000);
        loginpage.verify("Dashboard");
     });
-
 });
